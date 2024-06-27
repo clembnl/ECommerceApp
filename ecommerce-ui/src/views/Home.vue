46ba1baba54931c5ec4a30afb1c52fe9e743d544
clembnl <cl.bonal@free.fr>
2024-06-27 17:08:25 +0200

    home responsive

--- ecommerce-ui/src/views/Home.vue
+++ ecommerce-ui/src/views/Home.vue
@@ -211,4 +211,167 @@ export default {
 .transparent {
     opacity: 0.5;
 }
+
+@media (max-width: 1420px) {
+    #home .container {
+        width: 90%;
+    }
+}
+
+@media (max-width: 1265px) {
+    #home .container {
+        width: 93%;
+    }
+
+    #home .row {
+        justify-content: space-between;
+    }
+}
+
+@media (max-width: 1225px) {
+    #home .row {
+        justify-content: center;
+    }
+
+    .card {
+        width: 300px !important;
+    }
+
+    .card .card-img img {
+        width: 300px !important;
+        height: 300px !important;
+    }
+}
+
+@media (max-width: 1064px) {
+    .card {
+        width: 280px !important;
+    }
+
+    .card .card-img img {
+        width: 280px !important;
+        height: 280px !important;
+    }
+}
+
+@media (max-width: 1000px) {
+    .filters {
+        margin: 10px 100px 30px 100px !important;
+    }
+
+    .card {
+        width: 260px !important;
+    }
+
+    .card .card-img img {
+        width: 260px !important;
+        height: 260px !important;
+    }
+}
+
+@media (max-width: 935px) {
+    .filters {
+        margin: 10px 80px 30px 80px !important;
+    }
+
+    .card {
+        width: 240px !important;
+    }
+
+    .card .card-img img {
+        width: 240px !important;
+        height: 240px !important;
+    }
+}
+
+@media (max-width: 870px) {
+    .filters {
+        margin: 10px 25px 30px 25px !important;
+    }
+
+    .card {
+        width: 210px !important;
+    }
+
+    .card .card-img img {
+        width: 210px !important;
+        height: 210px !important;
+    }
+}
+
+@media (max-width: 760px) {
+    .filters {
+        margin: auto !important;
+    }
+
+    .filters ul {
+        flex-direction: column;
+    }
+
+    .filters li {
+        margin-top: 5px !important;
+        margin-bottom: 5px !important;
+        margin-left: auto !important;
+        margin-right: auto !important;
+    }
+}
+
+@media (max-width: 720px) {
+    #navbar-menu {
+        display: none !important;
+    }
+
+    .container-filtered {
+        display: none !important;
+    }
+
+    #container-all {
+        visibility: visible;
+    }
+}
+
+@media (max-width: 512px) {
+    .card {
+        width: 300px !important;
+    }
+
+    .card .card-img img {
+        width: 300px !important;
+        height: 300px !important;
+    }
+}
+
+@media (max-width: 425px) {
+    #navbar-icon #user {
+        margin-right: 5px !important;
+    }
+
+    #navbar-icon #cart {
+        margin-left: 5px !important;
+        margin-right: 0 !important;
+    }
+}
+
+@media (max-width: 370px) {
+    #navbar-icon #user {
+        margin-left: 5px !important;
+        margin-right: 0 !important;
+    }
+
+    #navbar-icon #cart {
+        margin-left: 0 !important;
+    }
+}
+
+@media (max-width: 360px) {
+    nav {
+        margin: 10px !important;
+    }
+}
+
+@media (max-width: 330px) {
+    nav {
+        margin: 0 !important;
+    }
+}
 </style>
